ec1748e3fa09fd58b4b303234965398620c0ce87
AndriiKot <a_kocyuba@ukr.net>
2023-03-25 10:24:11 +0100

    Creation Node.js Lesson 1

---
+++ app.js
@@ -0,0 +1,21 @@
+﻿console.log('go Node.js')
+let a;
+a = '10';
+console.log(a + 10) // '1010'
+
+let test = 'var test'
+
+// function test() {
+// console.log('function test');
+// }
+// SyntaxError
+
+function test_() {
+  console.log('function test');
+}
+
+test_();  // 'function test'
+console.log(test_());  // 'function test'; undefined
+console.log(test_);    // [Finction: test_]
+console.log(typeof(test_)) // function
+
