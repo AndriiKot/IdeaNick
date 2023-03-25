974c189a7e3c51675544279372b89f4ab628fc7b
AndriiKot <a_kocyuba@ukr.net>
2023-03-25 10:33:58 +0100

    Add function and arr Node.js

--- app.js
+++ app.js
@@ -14,8 +14,30 @@ function test_() {
   console.log('function test');
 }
 
-test_();  // 'function test'
-console.log(test_());  // 'function test'; undefined
-console.log(test_);    // [Finction: test_]
-console.log(typeof(test_)) // function
+test_();                    // 'function test'
+console.log(test_());       // 'function test'; undefined
+console.log(test_);         // [Finction: test_]
+console.log(typeof(test_) ) // function
+
+function b(){
+ console.log("Hello, Node.js")
+}
+
+const c = () => {
+ console.log('Hi, Node.js')
+}
+
+b();c();                      // 'Hello, Node.js'; 'Hi, Node.js'
+
+const myArray = [1,2,3,4]
+console.log(myArray[3])      // 4
+
+
+
+
+
+
+
+
+
 
